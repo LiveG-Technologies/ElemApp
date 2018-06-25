b0bfbd0ce5accd9fc17458ed2020cfacf8001049
LiveG <hi@liveg.tech>
2018-06-25 13:25:37 +0100

    Create sign in page

---
+++ src/user/script.js
@@ -0,0 +1,36 @@
+var currentUid = null;
+
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.search) || [null, ""])[1].replace(/\+/g, "%20")) || null;
+}
+
+function change(user) {
+    if (user && user.uid != null) {
+        // Signed in.
+        $(".myuid").text(currentUid);
+        
+        firebase.database().ref("users/" + user.uid + "/_settings/name").on("value", function(snapshot) {
+            if (getURLParameter("test") != "true") {
+                $(".myname").text(snapshot.val());
+            }
+        });
+    } else {
+        // Signed out.
+        $(".myuid").text("null");
+
+        if (getURLParameter("test") != "true") {
+            window.location.href = "../signin.html";
+        }
+    }
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+    // Checks if user auth state has changed.
+    if (user) {currentUid = user.uid;} else {currentUid = null;}
+
+    change(user);
+});
+
+function signOut() {
+    firebase.auth().signOut();
+}
