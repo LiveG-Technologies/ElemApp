cd7ce6956f10e1d2dc31ec1ed5432bf41a697406
LiveG <hi@liveg.tech>
2018-06-26 18:43:01 +0100

    Fix Firebase read timings

--- src/user/dashboard.js
+++ src/user/dashboard.js
@@ -9,20 +9,22 @@ function deleteApp(key) {
     firebase.database().ref("users/" + currentUid + "/apps/" + key).set(null);
 }
 
-firebase.database().ref("users/" + currentUid + "/apps").on("value", function(snapshot) {
-    $("#apps").html("");
+$(function() {
+    firebase.database().ref("users/" + currentUid + "/apps").on("value", function(snapshot) {
+        $("#apps").html("");
 
-    snapshot.forEach(function(childSnapshot) {
-        var childKey = childSnapshot.key;
-        var childData = childSnapshot.val();
+        snapshot.forEach(function(childSnapshot) {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
 
-        $("#apps").html($("#apps").html() + `
-            <div class="card inverse">
-                <h2><a href="#" class="inherit">` + childData.name.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&/g, "&amp;") + `</a></h2>
-                <div class="right">
-                <button class="bad" onclick="deleteApp('` + childKey + `');"><i class="fas fa-trash-alt"></i></button><button class="secondary">View</button><button>Edit</button>
+            $("#apps").html($("#apps").html() + `
+                <div class="card inverse">
+                    <h2><a href="#" class="inherit">` + childData.name.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&/g, "&amp;") + `</a></h2>
+                    <div class="right">
+                    <button class="bad" onclick="deleteApp('` + childKey + `');"><i class="fas fa-trash-alt"></i></button><button class="secondary">View</button><button>Edit</button>
+                    </div>
                 </div>
-            </div>
-        `);
+            `);
+        });
     });
 });
