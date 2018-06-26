abb3c133442a39bbbac0d9a777d6ceb87c5cd940
LiveG <hi@liveg.tech>
2018-06-26 18:36:04 +0100

    Fix retrieval of apps

--- src/user/dashboard.js
+++ src/user/dashboard.js
@@ -6,27 +6,23 @@ function addApp() {
 }
 
 function deleteApp(key) {
-    firebase.database().ref("users/" + currentUid + "/apps/" + key).delete().then(setApps);
+    firebase.database().ref("users/" + currentUid + "/apps/" + key).delete();
 }
 
-function setApps() {
-    firebase.database().ref("users/" + currentUid + "/apps").on("value", function(snapshot) {
-        $("#apps").html("");
+firebase.database().ref("users/" + currentUid + "/apps").on("value", function(snapshot) {
+    $("#apps").html("");
 
-        snapshot.forEach(function(childSnapshot) {
-            var childKey = childSnapshot.key;
-            var childData = childSnapshot.val();
+    snapshot.forEach(function(childSnapshot) {
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
 
-            $("#apps").html($("#apps").html() + `
-                <div class="card inverse">
-                    <h2><a href="#" class="inherit">` + childSnapshot.name.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&/g, "&amp;") + `</a></h2>
-                    <div class="right">
-                    <button class="bad" onclick="deleteApp('` + childKey + `');"><i class="fas fa-trash-alt"></i></button><button class="secondary">View</button><button>Edit</button>
-                    </div>
+        $("#apps").html($("#apps").html() + `
+            <div class="card inverse">
+                <h2><a href="#" class="inherit">` + childData.name.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&/g, "&amp;") + `</a></h2>
+                <div class="right">
+                <button class="bad" onclick="deleteApp('` + childKey + `');"><i class="fas fa-trash-alt"></i></button><button class="secondary">View</button><button>Edit</button>
                 </div>
-            `);
-        });
+            </div>
+        `);
     });
-}
-
-setApps();
+});
